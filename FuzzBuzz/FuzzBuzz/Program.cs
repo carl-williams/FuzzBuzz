5a65e32aa92076e70a445d1c5f3b556d8eaf1064
Carl williams <carl.williams@halofinancial.com>
2024-03-12 15:05:13 +0000

    Console app completed

--- FuzzBuzz/FuzzBuzz/Program.cs
+++ FuzzBuzz/FuzzBuzz/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FuzzBuzz.Interfaces;
+using System.Reflection;
+
+Console.WriteLine("Welcome to Fuzz Buzz Number encoder");
+var engine = Assembly.GetExecutingAssembly().CreateInstance("FuzzBuzz.BL.FuzzBuzzGame") as IFuzzBuzzGame;
+if (engine == null)
+{
+    throw new Exception("Unable to find FuzzBuzz game engine");
+}
+
+var done = false;
+while (!done)
+{
+    Console.WriteLine();
+    Console.WriteLine("Enter number list delimited by a space, or press return to exit");
+    var numberString = Console.ReadLine();
+    if (string.IsNullOrEmpty(numberString))
+    {
+        done = true;
+    }
+    else
+    {
+        var numbers = numberString.Split(' ')
+            .Select(ns =>
+            {
+                var n = 0;
+                int.TryParse(ns, out n);
+                return n;
+            })
+             .ToList();
+        Console.WriteLine(engine.GetTextForNumberEnumerable(numbers));
+    }
+}
+
+
